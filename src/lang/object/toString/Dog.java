c415d8b0bbf1a39c5ad42647f3db03c6303db25f
ENFJ <33335762+choisunghwan@users.noreply.github.com>
2025-02-06 18:47:39 +0900

    # toString()

---
+++ src/lang/object/toString/Dog.java
@@ -0,0 +1,15 @@
+package lang.object.toString;
+
+public class Dog {
+    private String dogName;
+    private int age;
+
+    public Dog(String dogName, int age) {
+        this.dogName = dogName;
+        this.age = age;
+    }
+    public String toString() {
+        return "dogName=" + dogName + "/" + "age" + age;
+
+    }
+}
