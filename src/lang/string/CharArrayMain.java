e6e43c9ef738dc57bcefac4861ff3cfbdfc5b5d4
ENFJ <33335762+choisunghwan@users.noreply.github.com>
2025-02-11 14:55:19 +0900

    # String 클래스 -기본

---
+++ src/lang/string/CharArrayMain.java
@@ -0,0 +1,14 @@
+package lang.string;
+
+public class CharArrayMain {
+    public static void main(String[] args) {
+        char a = '가';
+        System.out.println("a = " + a);
+
+        String str = "hello";
+        System.out.println("str = " + str);
+
+    }
+
+
+}
