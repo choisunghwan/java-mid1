117f5dd34e0f9d0e2c5ba617b0e6ef1ec7f21abf
ENFJ <33335762+choisunghwan@users.noreply.github.com>
2025-02-12 11:06:07 +0900

    # String 클래스 -주요메서드2

--- src/lang/string/method/StringChangeMain1.java
+++ src/lang/string/method/StringChangeMain1.java
@@ -8,6 +8,8 @@ public class StringChangeMain1 {
         System.out.println("인덱스 7부터 12까지의 부분 문자열: " + str.substring(7, 12));
         System.out.println("문자열 결합: " + str.concat("!!!"));
         System.out.println("'Java'를 'World'로 대체: " + str.replace("Java", "World"));
+        System.out.println("첫번째 'Java'를 'World'로 대체: " + str.replaceFirst("Java", "World"));
+
 
     }
 }
