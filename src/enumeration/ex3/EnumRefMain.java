c715f2e7c73af0f5ee3d6975eda678853d7459f0
ENFJ <33335762+choisunghwan@users.noreply.github.com>
2025-02-24 15:39:21 +0900

    # 열거형 Enum 타입

---
+++ src/enumeration/ex3/EnumRefMain.java
@@ -0,0 +1,11 @@
+package enumeration.ex3;
+
+public class EnumRefMain {
+    public static void main(String[] args) {
+        System.out.println("class BASIC = " + Grade.BASIC.getClass());
+        System.out.println("class GOLD = " + Grade.GOLD.getClass());
+        System.out.println("class DIAMOND = " + Grade.DIAMOND.getClass());
+
+        System.out.println("ref BASIC = " + Grade.BASIC);
+    }
+}
